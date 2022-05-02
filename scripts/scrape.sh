1fe735eb6700fbed7df3d269a067a47aa28cb445
Lukács Zsolt <rockdonald2@gmail.com>
2022-05-02 21:12:20 +0300

    data pull, updated scrape script to display timestamps and generalized crude scrape method, now it will scrape for yesterday's prices

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -6,19 +6,19 @@ do
 
     if ! jupyter nbconvert --execute /home/jovyan/work/workspace/scrape.ipynb --to notebook --ExecutePreprocessor.timeout=-1
     then
-        echo 'Failed to execute scrape notebook.' >> /home/jovyan/work/workspace/error
+        echo "Failed to execute scrape notebook at $(date)." >> /home/jovyan/work/workspace/error
         exit 1
     fi
 
     if ! jupyter nbconvert --execute /home/jovyan/work/workspace/upload_gas.ipynb --to notebook --ExecutePreprocessor.timeout=-1
     then
-        echo 'Failed to execute upload notebook.' >> /home/jovyan/work/workspace/error
+        echo "Failed to execute upload gas notebook at $(date)." >> /home/jovyan/work/workspace/error
         exit 1
     fi
 
     if ! jupyter nbconvert --execute /home/jovyan/work/workspace/upload_crude.ipynb --to notebook --ExecutePreprocessor.timeout=-1
     then
-        echo 'Failed to execute upload notebook.' >> /home/jovyan/work/workspace/error
+        echo "Failed to execute upload gas notebook at $(date)." >> /home/jovyan/work/workspace/error
         exit 1
     fi
 
@@ -29,3 +29,4 @@ do
     echo "Next scrape for GasPrice in a day." >> /home/jovyan/work/workspace/log
     sleep 1d
 done
+
