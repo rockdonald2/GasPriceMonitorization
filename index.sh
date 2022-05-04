ae4cb0b15e909f03825eabb4bff3628984f5ba3d
Lukács Zsolt <rockdonald2@gmail.com>
2022-05-04 18:09:02 +0300

    minor changes

--- index.sh
+++ index.sh
@@ -7,7 +7,7 @@ mkdir logs 2>>/dev/null
 echo '' > logs/log.log
 echo '' > logs/error.log
 
-if ! docker compose up -d --force-recreate 1>logs/log.log 2>logs/error.log
+if ! docker compose up -d 1>logs/log.log 2>logs/error.log
 then
     echo "Failed to create containers."
     exit 1
