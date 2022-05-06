1e17983e71abf2913b9e49e8817077477cd2a462
Lukács Zsolt <rockdonald2@gmail.com>
2022-05-06 20:06:17 +0300

    updated scrape script msgs and skip Volume column when casting to float

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -20,15 +20,15 @@ do
 
     if ! jupyter nbconvert --execute $ROOT/upload_crude.ipynb --to notebook --ExecutePreprocessor.timeout=-1
     then
-        echo "Failed to execute upload gas notebook at $(date)." >> $ROOT/error
+        echo "Failed to execute upload crude notebook at $(date)." >> $ROOT/error
         exit 1
     fi
 
     rm $ROOT/scrape.nbconvert.ipynb $ROOT/upload_gas.nbconvert.ipynb $ROOT/upload_crude.nbconvert.ipynb
 
-    echo "Finished scraping GasPrice data at $(date)." >> $ROOT/log
+    echo "Finished scraping GasPrice and CrudeOil data at $(date)." >> $ROOT/log
 
-    echo "Next scrape for GasPrice in a day." >> $ROOT/log
+    echo "Next scrape for GasPrice and CrudeOil in a day." >> $ROOT/log
     sleep 1d
 done
 
